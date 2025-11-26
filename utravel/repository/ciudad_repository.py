5d6ed1cbd3f8657c966d320172a7c63b3ac23072
David Niño <151692499+DaavidN1noo@users.noreply.github.com>
2025-11-26 15:04:13 -0500

    api, repository y service de Ciudades - David
    Se implementa la capa API, repository y service para el modelo Ciudad.
    Incluye métodos de creación, actualización, obtención por id y listado.

---
+++ utravel/repository/ciudad_repository.py
@@ -0,0 +1,54 @@
+from utravel.models import Ciudad
+from typing import Optional
+from django.db.models import QuerySet
+
+class CiudadRepository:
+
+    #Declarando una referencia a la clase no al objeto
+    model = Ciudad
+
+
+    #crear una ciudad
+    def create(self, **data) -> Ciudad: #recibir todos los datos por medio de un diccionario
+        return Ciudad.objects.create(**data) #Crear una nueva ciudad, objects.create() viene del ORM Django crea un neuvo registro en la bd
+    
+    #actualizar una ciudad
+    def update(self, id:int, **data) -> Optional[Ciudad]: #Optional es un tipo de dato que puede retornar un objeto o un None
+        ciudad_object = Ciudad.objects.filter(pk=id).first() #filtra por id y obtiene el primer resultado
+
+        #si no se encuentra
+        if not ciudad_object:
+            return None #None: tipo de dato void null o undefind no hay nada
+        
+        #si se encuentra
+        for field, value in data.items():
+            setattr(ciudad_object, field, value)
+        
+        #salvar y retornar
+        ciudad_object.save() #guarda los cambios del objeto en la bd
+        return ciudad_object
+    
+    #eliminar lógico ciudad
+    def desactivate(self, id:int) -> bool:
+        deleted: Ciudad.objects.filter(pk=id).update(ciudad_status="0")
+        return deleted > 0
+    
+    #listar todas las ciudades
+    def listCiudades(self) -> QuerySet[Ciudad]:
+        return(
+            self.model.objects.all()
+        )
+    
+    #listar para
+    def get_by_id(self, id:int) -> Optional[Ciudad]:
+        return Ciudad.objects.filter(pk=id).first()
+    
+    def get_by_name(self, name:str) -> Optional[Ciudad]:
+        return Ciudad.objects.filter(ciu_descripcion=name).first()
+
+
+    
+    
+
+
+
---
+++ utravel/service/ciudad_sercive.py
@@ -0,0 +1,76 @@
+from typing import Optional
+from utravel.repository.ciudad_repository import CiudadRepository
+from utravel.models import Ciudad
+from django.db.models import QuerySet
+
+class CiudadService:
+
+    #Contructor con inyeción del repository de ciudad con none = no retorna nada
+    def __init__(self, repository: Optional[CiudadRepository] = None) -> None:
+
+        self.repository = repository or CiudadRepository #Si no me pasan un repository instancio uno
+
+    
+    #Consultas de lectura ---------------------------------
+
+    #listar todas las ciudades
+    def list_ciudades(self) -> QuerySet[Ciudad]:
+        return self.repository.listCiudades() #método del repository
+    
+    #listar por ID
+    def get_ciudad_id(self, id:int)-> Optional[Ciudad]:
+        return self.repository.get_by_id(id)
+    
+    #listar por nombre
+    def get_ciudad_name(self, name:str)-> Optional[Ciudad]:
+        return self.repository.get_by_name(name)
+    
+    #métodos CRUD ---------------------------------------
+
+    #CREATE
+    def create_ciudad(self, **data) -> Ciudad:
+
+        #validar que el nombre/descripción no exista
+        name = data.get("ciu_descripcion") #trayendo dato del diccionario con clave: ciu_descripcion
+
+        #validar nombre/descripcion único
+        if name and self.repository.get_by_name(name): #si la info de la ciudad y en la bd ya existe un registro igual
+            raise ValueError("Ya existe una ciudad registrada con ese nombre") #Exepción
+        
+        #Estado por default
+        data.setdefault("ciudad_status", 1)
+
+        #Creación de la ciudad
+        return self.repository.create(**data)
+    
+    #UPDATE
+    def update_ciudad(self, id:int, **data) -> Optional[Ciudad]:
+
+        #validar que el nombre/descripción no exista
+        name = data.get("ciu_descripcion") #trayendo dato del diccionario con clave: ciu_descripcion
+        status = data.get("ciudad_status")
+
+        #validar que el status sea 1
+        if status != "1":
+            raise ValueError("El registro está inactivo. No se puede actualizar")
+
+        #validar nombre/descripcion único
+        if name and self.repository.get_by_name(name): #si la info de la ciudad y en la bd ya existe un registro igual
+            raise ValueError("Ya existe una ciudad registrada con ese nombre")
+        
+        return self.repository.update(id, **data)
+    
+    #DELETE LÓGICO
+    def desactivate(self, id: int) -> bool:
+        obj = self.repository.get_by_id(id)
+
+        if not obj:
+            return False
+        
+        return self.repository.desactivate(id)
+        
+    
+
+        
+
+
