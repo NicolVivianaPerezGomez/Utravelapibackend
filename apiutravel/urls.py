f2eea00e155fff3f7babd423736a39bbaf4613fa
Inge Ali <126697606+alinge20@users.noreply.github.com>
2025-11-30 22:41:07 -0500

    rutas1

--- apiutravel/urls.py
+++ apiutravel/urls.py
@@ -20,3 +20,21 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.urls import path
+from .views import (
+    listar_rutas,
+    crear_ruta,
+    obtener_ruta,
+    actualizar_ruta,
+    eliminar_ruta
+)
+
+urlpatterns = [
+    path("rutas/", listar_rutas, name="listar_rutas"),
+    path("rutas/crear/", crear_ruta, name="crear_ruta"),
+    path("rutas/<int:id>/", obtener_ruta, name="obtener_ruta"),
+    path("rutas/<int:id>/actualizar/", actualizar_ruta, name="actualizar_ruta"),
+    path("rutas/<int:id>/eliminar/", eliminar_ruta, name="eliminar_ruta"),
+]
+
--- utravel/views.py
+++ utravel/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import RutaTuristica
+from .serializers  import RutaTuristicaSerializer
+
+
+@api_view(['GET'])
+def listar_rutas(request):
+    rutas = RutaTuristica.objects.filter(rut_estado="1")
+    serializer = RutaTuristicaSerializer(rutas, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def crear_ruta(request):
+    serializer = RutaTuristicaSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(rut_estado="1") 
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def obtener_ruta(request, id):
+    try:
+        ruta = RutaTuristica.objects.get(rut_id=id, rut_estado="1")
+    except RutaTuristica.DoesNotExist:
+        return Response({"error": "Ruta no encontrada o inactiva."},
+                        status=status.HTTP_404_NOT_FOUND)
+
+    serializer = RutaTuristicaSerializer(ruta)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def actualizar_ruta(request, id):
+    try:
+        ruta = RutaTuristica.objects.get(rut_id=id, rut_estado="1")
+    except RutaTuristica.DoesNotExist:
+        return Response({"error": "Ruta no encontrada."}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = RutaTuristicaSerializer(ruta, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def eliminar_ruta(request, id):
+    try:
+        ruta = RutaTuristica.objects.get(rut_id=id, rut_estado="1")
+    except RutaTuristica.DoesNotExist:
+        return Response({"error": "Ruta no encontrada."}, status=status.HTTP_404_NOT_FOUND)
+
+    ruta.rut_estado = "0"
+    ruta.save()
+
+    return Response({"mensaje": "Ruta desactivada correctamente."})
