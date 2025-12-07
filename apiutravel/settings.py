f777be10b6d0a4be1020ae4129901d0ad67eec6e
David Niño <151692499+DaavidN1noo@users.noreply.github.com>
2025-12-06 21:26:27 -0500

    instalacion de JWT en el proyecto
    IMPORTANE. modificado settings

--- apiutravel/settings.py
+++ apiutravel/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'utravel',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+     
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
