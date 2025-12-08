7c8e3d8791835e2cee2a824aeeb87b7dbfff3fc7
David Niño <151692499+DaavidN1noo@users.noreply.github.com>
2025-12-08 12:57:45 -0500

    pruebas de desempeño
    agregue la capreta test_perfomance donde estan las pruebas de desempeño

---
+++ utravel/test_performance/locustfile.py
@@ -0,0 +1,77 @@
+from locust import HttpUser, task, between
+import random
+
+
+#PRUEBAS DE DESEMEPEÑO (TIEMPO DE RESPUESTA) CON LOCUST
+"""Evalue el rendimiento de una app simulando a miles de usuarios usandola al mismo tiempo para 
+ver el tiempo de respuesta de esta
+
+pesos:
+3 = se ejecuta más veces cada x tiempo
+1 = se ejecuta menos veces cada x tiempo
+"""
+
+class CiudadSimulada(HttpUser):
+
+    wait_time = between(1,3) #tiempo de espera entre peticiones del usuario (crear, editar, listar)
+
+    ciudad_creada_id = None #variable para almecenar el ID generado
+
+    #método para generar JWT
+    def on_start(self):
+
+        self.token = None #declaracion vacia
+        self.headers = {} #declaracion vacia
+
+        #obtener token
+        response = self.client.post("/api/token/", json={ 
+            "username": "root",
+            "password": "1234"
+        })
+
+        #detener si no se obtiene token
+        if response.status_code != 200:
+            print("ERROR TOKEN: ", response.status_code, response.text)
+            return
+        
+        #Guardar el token para futuras peticiones
+        self.token = response.json().get("access") #se accede al acces (token generado)
+        self.headers = {"Authorization": f"Bearer {self.token}"} #se gurada en un diccionario
+
+    @task(3)
+    def listar(self):
+        self.client.get("/ciudades/", headers=self.headers)
+
+    @task(1) #decorador que indica que esta accion la eejcuta un usuario con un peso de 1 
+    def crear_ciudad(self):
+
+        ciu_descripcion= f"Ciudad de prueba {random.randint(100,999)}" #guarando un ejemplo de registro con un nombre con int random
+
+        response = self.client.post("/ciudades/", json={ #método simulado POST con el dato
+            "ciu_descripcion": ciu_descripcion},
+            headers=self.headers
+            )
+
+        if response.status_code in (200, 201): #CODIGO DE OK o Created
+            try:
+                self.ciudad_creada_id = response.json().get("id") #response json: convertir JSON a diccionario y .get("id") extrae el ID
+            except:
+                pass #si no se puede igual pasa y continua 
+
+    @task(1)
+    def buscar_por_id(self):
+        if self.ciudad_creada_id:
+            self.client.get(f"/ciudades/{self.ciudad_creada_id}/", headers=self.headers)
+
+    @task(1)
+    def actualizar_ciudad(self):
+        if self.ciudad_creada_id:
+            self.client.put(f"/ciudades/{self.ciudad_creada_id}/", json={
+                "ciu_descripcion": "Ciudad Actualizada"},
+                headers=self.headers)
+
+    @task(1)
+    def buscar_por_nombre(self):
+        self.client.get("/ciudades/Bogot%C3%A1/", headers=self.headers)
+
+
