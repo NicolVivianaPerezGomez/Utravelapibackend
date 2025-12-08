9611fdefa1300b60711b38c65317b2aae979bdf4
David Niño <151692499+DaavidN1noo@users.noreply.github.com>
2025-12-08 16:07:05 -0500

    PRUEBAS DE desempeño de Tipos de experiencia completas
    finalizado archivo de pruebas de desempeño tp de experiencia

--- utravel/test_performance/tpExperiencia_locust.py
+++ utravel/test_performance/tpExperiencia_locust.py
@@ -3,8 +3,7 @@ import random
 import string
 import threading
 
-
-#PRUEBAS DE DESEMEPEÑO (TIEMPO DE RESPUESTA) CON LOCUST CIUDADES
+#PRUEBAS DE DESEMEPEÑO (TIEMPO DE RESPUESTA) CON LOCUST TIPO DE EXPERIENCIAS
 """Evalue el rendimiento de una app simulando a miles de usuarios usandola al mismo tiempo para 
 ver el tiempo de respuesta de esta
 
@@ -13,17 +12,16 @@ pesos:
 1 = se ejecuta menos veces cada x tiempo
 """
 
-TOKEN_GLOBAL = None #variable vacio donde se almacena el token
+TOKEN_GLOBAL = None 
 TOKEN_LOCK = threading.Lock() #evita que el token se genere simultaneamente (solo permite 1) solo 1 hilo
 
+class TpExperiencia(HttpUser): #HttpUser usuario virtual con peticion http
 
-class CiudadSimulada(HttpUser): #HttpUser usuario virtual con peticion http
+    wait_time = between(1,3) #tiempo de espera entre peticiones
 
-    wait_time = between(1,3) #tiempo de espera entre peticiones del usuario (crear, editar, listar)
+    experiencia_creada_id = None 
 
-    ciudad_creada_id = None #variable para almecenar el ID generado
-
-    #MÉTODO PARA OBTENER EL TOKEN
+    #MÉTODO PARA OBTENER TOKEN
     def on_start(self):
         global TOKEN_GLOBAL
 
@@ -45,11 +43,11 @@ class CiudadSimulada(HttpUser): #HttpUser usuario virtual con peticion http
 
         self.headers = {"Authorization": f"Bearer {TOKEN_GLOBAL}"}
 
- 
-    #MÉTODO PARA PROBAR LISTAR CIUDADES
+    #MÉTODO PARA PROBAR LISTAR EXPERIENCIAS
+
     @task(3) #decorador que indica que esta accion la eejcuta un usuario con un peso de 1 
     def listar(self):
-        self.client.get("/ciudades/", headers=self.headers) #client hace referencia al cliente
+        self.client.get("/tipo_exp/", headers=self.headers) #client hace referencia al cliente
         #headers es propio del get header: cabeceras personalizadas como ej tokens
 
     #metodo para generar nombre aleatorio
@@ -59,44 +57,46 @@ class CiudadSimulada(HttpUser): #HttpUser usuario virtual con peticion http
                                             #String: modulo para trabajar con cad. de texto
 
         return ''.join(random.choice(letras) for _ in range(longitud)) #bucle donde no importa el indice _ que retorna una cadena de texto '' con el método random.choice() que genera conjuntos de caracteres random
+    
+    #MÉTODO PARA CREAR EXPERIENCIAS 
 
-    #MÉTODO PARA PROBAR CREAR CIUDADES
-    @task(1) 
-    def crear_ciudad(self):
+    @task(1)
+    def crear_experiencias(self):
 
-        ciu_descripcion = f"Ciudad {self.nombre_valido(5)}"#guarando un ejemplo de registro con un nombre con método de random string
+        tipexp_descripcion= f"Experiencia {self.nombre_valido(5)}"#guarando un ejemplo de registro con un nombre con método de random string
 
-        response = self.client.post("/ciudades/", json={ #método simulado POST con el dato
-            "ciu_descripcion": ciu_descripcion
-            }, headers=self.headers)
-        
-        if response.status_code in (200, 201): #CODIGO DE OK o Created
+        response = self.client.post("/tipo_exp/", json={
+            "tipexp_descripcion": tipexp_descripcion
+        }, headers=self.headers)
+
+        if response.status_code in (200, 201):
+
             try:
-                self.ciudad_creada_id = response.json().get("ciu_id") #response json: convertir JSON a diccionario y .get("id") extrae el ID
+                self.experiencia_creada_id = response.json.get("tipexp_id")
             except:
-                pass #si no se puede igual pasa y continua 
-
+                pass #si no se puede igual pasa y continua
+    
     #MÉTODO PARA PROBAR BUSCAR POR ID
     @task(1)
     def buscar_por_id(self):
-        if self.ciudad_creada_id:
-            self.client.get(f"/ciudades/{self.ciudad_creada_id}/", headers=self.headers)
+        if self.experiencia_creada_id:
+            self.client.get(f"/tipo_exp/{self.experiencia_creada_id}/", headers=self.headers)
 
-    #MÉTODO PARA PROBAR ACTUALIZAR 
+    #MÉTODO PARA PROBAR ACTUALIZAR
     @task(1)
-    def actualizar_ciudad(self):
+    def actualizar_experiencia(self):
 
-        new_description = f"Ciudad {self.nombre_valido(5)}" #nuevo nombre random
+        new_descripcion = f"Experiencia {self.nombre_valido(5)}" #nuevo nombre random
 
-        if self.ciudad_creada_id:
-            self.client.put(f"/ciudades/{self.ciudad_creada_id}/", json={
-                "ciu_descripcion": new_description},
-                headers=self.headers)
- 
-    #MÉTODO PARA PROBAR BUSCAR POR NOMBRE
+        if self.experiencia_creada_id:
+            self.client.put(f"/tipo_exp/{self.experiencia_creada_id}/", json={
+                "tipexp_descripcion": new_descripcion
+            }, headers=self.headers)
+
+    #método para buscar por nombre
     @task(1)
     def buscar_por_nombre(self):
-        self.client.get("/ciudades/Bogot%C3%A1/", headers=self.headers)
+        self.client.get("/tipo_exp/Aventura/", headers=self.headers)
 
 
 """
@@ -104,8 +104,9 @@ Como ejecutar:
 
 correr servido: py manage.py runserver
 buscar en la terminar la carpeta test_performance
-ejecutar locust -f locustfile.py --host=http://127.0.0.1:8000
+ejecutar locust -f tpExperiencia_locust.py --host=http://127.0.0.1:8000
 abrir en la web http://localhost:8089
 ejecutar prueba
 
 """
+
